ce78887d5f477ad2f979c9c01b859d87a4479ddb
nisarg0016 <nisargdesai0016@gmail.com>
2025-09-30 18:56:41 +0400

    Revert "digital mess-card backend"
    This reverts commit 406db87b30bbf8348edea7619d10cb546f9439ab.

--- middleware/apikeyAuth.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function(req, res, next) {
-  const apiKey = req.header('x-api-key');
-  if (!apiKey || apiKey !== process.env.API_KEY) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  next();
-};
